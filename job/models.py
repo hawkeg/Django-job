60a14bb7c39950a3aca713849649179f641126a4
hawkeg <mrdos9@gmail.com>
2022-03-14 15:24:06 +0300

    eidt job  modle

--- job/models.py
+++ job/models.py
@@ -1,3 +1,21 @@
+from email.policy import default
+from random import choice, choices
 from django.db import models
 
 # Create your models here.
+JOB_TYPE= (
+    ('Full Time','Full Time'),
+    ('Part Time','Part Time'),
+)
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    # location
+    job_type = models.CharField(max_length=15, choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    Vacancy = models.IntegerField(default=1)
+    salary = models.FloatField(default=0.00)
+    expeince = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
