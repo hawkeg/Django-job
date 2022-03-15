b18c6adc3cfbfac806fd264864f7a66f790139a1
hawkeg <mrdos9@gmail.com>
2022-03-15 13:24:33 +0300

    add views

--- job/models.py
+++ job/models.py
@@ -13,9 +13,17 @@ class Job(models.Model):
     job_type = models.CharField(max_length=15, choices=JOB_TYPE)
     description = models.TextField(max_length=1000)
     published_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
     Vacancy = models.IntegerField(default=1)
     salary = models.FloatField(default=0.00)
     expeince = models.IntegerField(default=1)
 
+   # title of Record
     def __str__(self):
         return self.title
+
+class Category(models.Model):
+    name=models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
